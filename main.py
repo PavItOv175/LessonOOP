113d91abea86455d58ad1befe06d1c91a02c58c4
NIITZI\apavlov <apavlov@niitzi.by>
2022-07-20 13:52:50 +0300

    Исправлена реализация класса Bla()

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 #Создайте класс машина, и несколько экземпляров данного класса с разными атрибутами. Код разметите в гит репозитории и ссылочку мне на него.
 class Car:
-    wheel = 4
-    color = 'green'
-    doors = 4
+    def Bla(self):
+        wheel = 4
+        color = 'green'
+        doors = 4
+  #      return(self)
 Peugeot = Car()
 Peugeot.fogLights = 2
 Peugeot.multimediaOld = 1
+Peugeot.color = 'green'
 Lexus = Car()
 Lexus.type = 'Сrossover' # седан, универсал, кроссовер, внедорожник и т.д.
 Lexus.hoist = 2          # лебёдка
+Lexus.doors = 5
 BMW = Car()
 BMW.motor = '300 hp'     # 300 лошадиных сил
 BMW.antifly = 1          # Антикрыло открывается на скорости
-
-print(Peugeot.color, Peugeot.fogLights, Peugeot.multimediaOld)
+BMW.color = 'blue'
+BMW.doors = 2
+print(Peugeot.fogLights, Peugeot.multimediaOld)
 print(Lexus.type, Lexus.hoist)
 print(BMW.motor, BMW.antifly)
