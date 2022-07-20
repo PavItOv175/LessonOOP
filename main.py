cf9f7630bebd507cd1a53a83eb4458db09fb3019
NIITZI\apavlov <apavlov@niitzi.by>
2022-07-20 14:39:36 +0300

    Исправлена реализация класса Car

--- main.py
+++ main.py
@@ -1,23 +1,27 @@
 #Создайте класс машина, и несколько экземпляров данного класса с разными атрибутами. Код разметите в гит репозитории и ссылочку мне на него.
 class Car:
-    def Bla(self):
-        wheel = 4
-        color = 'green'
-        doors = 4
-  #      return(self)
-Peugeot = Car()
-Peugeot.fogLights = 2
-Peugeot.multimediaOld = 1
-Peugeot.color = 'green'
-Lexus = Car()
-Lexus.type = 'Сrossover' # седан, универсал, кроссовер, внедорожник и т.д.
-Lexus.hoist = 2          # лебёдка
-Lexus.doors = 5
-BMW = Car()
-BMW.motor = '300 hp'     # 300 лошадиных сил
-BMW.antifly = 1          # Антикрыло открывается на скорости
-BMW.color = 'blue'
-BMW.doors = 2
-print(Peugeot.fogLights, Peugeot.multimediaOld)
-print(Lexus.type, Lexus.hoist)
-print(BMW.motor, BMW.antifly)
+    emp_count = 0
+
+    def __init__(self, my_type, wheel, color, doors):
+        self.my_type = my_type
+        self.wheel = wheel
+        self.color = color
+        self.doors = doors
+        Car.emp_count += 1
+
+    def display_count(self):
+        print('Всего машин: %d' % Car.empCount)
+
+    def display_employee(self):
+        print('Тип кузова: {}. Кол-во колес: {}. Цвет машины: {}. Кол-во дверей: {}'.format(self.my_type, self.wheel, self.color, self.doors))
+
+Peugeot = Car('Седан', 4, 'Зелёный', 3)
+Peugeot.display_employee()
+
+Lexus = Car('Кросссовер', 4, 'Чёрный', 5)
+Lexus.display_employee()
+
+BMW = Car('Универсал', 4, 'Серебристый металлик', 5)
+BMW.display_employee()
+
+print(Car.emp_count)
